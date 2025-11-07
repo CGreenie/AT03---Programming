85ceeb1d3c133dc46a49d93bfadb61ee14fea49e
CGreenie <callumg345@gmail.com>
2025-11-07 13:06:35 +0800

    Added Camera Follow
    - Camera now follows on mouse movement

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Transform player;
+    public Vector3 offset = new Vector3(0f, 2f, -5f);   // Position behind player
+    public float smoothSpeed = 0.125f;                  // Smoother interpolation
+
+    private void LateUpdate()                           // Late update for movement to happen first
     {
-        
+        Vector3 desiredPosition = player.position + player.rotation * offset;                           // Follows the movement and applies offset
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(player.position);              // Cam looks at player
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
