a59d8dbf307457f3807957d2d211de2ed8783286
CGreenie <callumg345@gmail.com>
2025-11-11 10:55:23 +0800

    Added Camera and Player Interaction
    - Merged with dev_branch for new camera
    - Added raycasting to both player and camera

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -1,11 +1,13 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    public float reach = 3f;            // Interaction Distance
+    public float reach = 8f;            // Interaction Distance
+    public float reachPlayer = 2f;
 
-    //public Image crosshair;           // Crosshair Link
+    public Image crosshair;           // Crosshair Link
     //public Text infoPanel;            // Text UI Link
     void Start()
     {
@@ -14,6 +16,26 @@ public class PlayerInteraction : MonoBehaviour
 
     void Update()
     {
-        Debug.DrawRay(this.transform.position, this.transform.forward * reach, Color.blue, 0.01f);
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * reach, Color.blue, 0.01f);
+
+        Debug.DrawRay(this.transform.position, this.transform.forward * reach, Color.red, 0.01f);
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit objectHit, reach) == true)
+        {
+            Debug.Log($"{objectHit.collider.name} detected.");
+        }
+        else
+        {
+            Debug.Log("No object in range.");
+        }
+
+        //if (Physics.Raycast(this.transform.position, this.transform.forward, out RaycastHit objectHitPlayer, reachPlayer) == true)
+        //{
+        //    Debug.Log($"{objectHitPlayer.collider.name} detected.");
+        //}
+        //else
+        //{
+        //    Debug.Log("No object in range.");
+        //}
     }
 }
